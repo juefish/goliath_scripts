d6c3778ed45b1f0412c67ded9e05cf5e48059b0f
Dr. JueFish <juefish@users.noreply.github.com>
2021-10-05 13:25:32 -0400

    Update fastaExtract.py

--- fastaExtract.py
+++ fastaExtract.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 import sys
 import os
-#import set
 
-#from set import Set
+#Script to extract sequences from a larger fasta file using a list of sequence names that you either want to include or exclude from your final result.
+#Output is in STOUT so use '>' to redirect it to a new file.
 
 # Usage.
 if len(sys.argv) < 2:
-        print ""
-        print "This program extracts fasta sequences from a file by either exluding or including sequence based on a provide list of sequence names"
-        print "Usage: %s -i/e -list file1 -fasta file2"
-        print "-list: list of sequence names"
-        print "-fasta: fasta file"
-        print "-i: include list contents"
-	print "-e: exclude list contents"
-        print ""
+        print("")
+        print("This program extracts fasta sequences from a file by either exluding or including sequence based on a provide list of sequence names")
+        print("Usage: %s -i/e -list file1 -fasta file2")
+        print("-list: list of sequence names")
+        print("-fasta: fasta file")
+        print("-i: include list contents")
+	print("-e: exclude list contents")
+        print("")
         sys.exit()
 
 # Parse args.
@@ -35,7 +35,6 @@ name = set([])
 sequence = str()
 proceed = False
 fin2 = open(fls[1], "r")
-#fin1 = open(fls[0], "r")
 prev = str()
 counter = 0
 #print infile1
@@ -49,7 +48,6 @@ if(fls[0].count('#')<1):
     fin1.close()
 else:
     geneContigs.add(fls[0].strip('"').rstrip('#'))
-#print list(geneContigs)
 
 # extract contigs from larger file inclusion
 if(switch==True):
@@ -58,13 +56,13 @@ if(switch==True):
 		name = line.lstrip('>').split('|')
 	else:
 		name = line.lstrip('>').split()
-	#print name
+	
         if((str(name[0]) in geneContigs) == True):
 	    if len(sequence) > 0:
-                print sequence
+                print(sequence)
 	        sequence = ""
             proceed = True
-	    print ">%s" % (name[0])	
+	    print(">%s" % (name[0]))	
             continue
         
         elif(proceed == True)&(line[0]!='>'):
@@ -72,7 +70,7 @@ if(switch==True):
 	    continue
         else:
             proceed = False
-    print sequence
+    print(sequence)
 
 # extract contigs from larger file exclusion
 elif(switch==False):
@@ -83,10 +81,10 @@ elif(switch==False):
 		name = line.lstrip('>').split()
         if(((str(name[0]) in geneContigs) == False) and (line[0]=='>')):
 	    if len(sequence) > 0:
-                print sequence
+                print(sequence)
                 sequence = ""
             proceed = True
-            print ">%s" % (name)
+            print(">%s" % (name))
             continue
         
         elif(proceed == True)&(line[0]!='>'):
@@ -94,6 +92,6 @@ elif(switch==False):
             continue
         else:
             proceed = False
-    print sequence    
+    print(sequence)    
 fin2.close()
 
